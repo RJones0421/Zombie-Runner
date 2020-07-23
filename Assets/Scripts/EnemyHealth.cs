e731fdb83a351eb7b83e04debc0eb2ffbc3e1d6b
RJones0421 <rjcj0421@gmail.com>
2020-07-23 16:28:02 -0400

    Added Effects for Weapon
    Added a muzzle flash and sparks when bullet hits a wall.

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] float hitPoints = 100f;
+
+    public void TakeDamage(float weaponDamage)
     {
-        
+        hitPoints -= weaponDamage;
+        if (hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
 
     [SerializeField] Camera FPCamera = null;
     [SerializeField] float range = 100f;
+    [SerializeField] float damage = 10f;
+    [SerializeField] ParticleSystem muzzleFlash = null;
+    [SerializeField] GameObject hitEffect = null;
 
     void Update()
     {
@@ -18,8 +19,34 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        print(hit.transform.name);
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            print(hit.transform.name);
+
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null)
+            {
+                GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impact, 0.1f);
+                return;
+            }
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
     }
 }
