1958f9c3e698ce655d1b736ca3ba7d8e2566b685
RJones0421 <rjcj0421@gmail.com>
2020-07-24 18:29:39 -0400

    Added Basic Weapon Zoom
    Weapon can now zoom in, more features to be added.

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+
+    [SerializeField] float zoomedOutFOV = 30f;
+    [SerializeField] float zoomedInFOV = 60f;
+
+    Camera FPCamera;
+    bool isZoomedIn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPCamera = GetComponentInChildren<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Zoom();
+        }
+    }
+
+    private void Zoom()
+    {
+        isZoomedIn = !isZoomedIn;
+
+        if (isZoomedIn)
+        {
+            FPCamera.fieldOfView = zoomedInFOV;
+        }
+        else
+        {
+            FPCamera.fieldOfView = zoomedOutFOV;
+        }
+    }
+}
